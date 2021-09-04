dcd62618e760f0b4ad54c0fd556d73623d5bde51
Adrian Gonzalez <adriangonzalezsanchez1996@gmail.com>
2021-09-04 12:31:59 -0400

    Add task file

---
+++ tasks.py
@@ -0,0 +1,15 @@
+from invoke import task
+import os
+import shutil
+
+
+@task
+def clean(c):
+    def dir_walk(path: str):
+        for entry in os.scandir(path):
+            if entry.name == "__pycache__":
+                shutil.rmtree(entry.path)
+            elif entry.is_dir():
+                dir_walk(entry.path)
+
+    dir_walk(os.path.abspath("src"))
